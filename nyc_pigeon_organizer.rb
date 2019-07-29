616cb9ea85a3d1bd225fc8827eda5a46016cd012
George Kirby <georgeskirby@btinternet.com>
2019-07-29 15:44:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  pigeon_list = data.reduce({}) do |memo, (key, value)|
+    attribute = key
+    for actual_att in value
+      names_of_actual_att = value[actual_att]
+      for name in names_of_actual_att
+        if memo.include?name
+          
+          
+        else
+          #creates a new pair in memo for this pigeon
+          memo[name] = {attribute => [actual_att]
+        end
+      end
+    
+    end
+    
+    
+    memo
+  end
+  
+  pigeon_list
 end
