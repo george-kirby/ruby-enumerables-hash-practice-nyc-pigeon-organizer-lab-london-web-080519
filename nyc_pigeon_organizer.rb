87be354f00f40102c458f3281e60ef3c0bf491f8
George Kirby <georgeskirby@btinternet.com>
2019-07-29 16:04:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,24 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
-  pigeon_list = data.reduce({}) do |memo, (key, value)|
-    attribute = key
-    for actual_att in value
-      names_of_actual_att = value[actual_att]
-      for name in names_of_actual_att
+  pigeon_list = data.reduce({}) do |memo, (attribute, details)|
+    p attribute
+    p details
+    details.each do |qual_att, names_of_qual_att|
+      for name in names_of_qual_att
         if memo.include?name
-          
+          if memo[name].include?attribute
+            memo[name][attribute].push(qual_att.to_s)
+          else
+            memo[name][attribute] = [qual_att.to_s]
+          end
           
         else
-          #creates a new pair in memo for this pigeon
-          memo[name] = {attribute => [actual_att.to_s]
+          #creates a new pair in memo for this pigeon. :)
+          memo[name] = {attribute => [qual_att.to_s]}
         end
+      
       end
-    
     end
     
     
